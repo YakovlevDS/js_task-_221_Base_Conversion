a506d760405dd7f2e42f215382fd0b6c2c36bdbf
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-30 22:10:19 +0200

    feat:add solutions

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 // solution 1
-// function isPrime(num) {
-//       for(let i =2, s=Math.sqrt(num);i<=s; i++){
-//         if(num%i===0) return false;
-//       }
-//       return num>1;
-//      }
+function convert(input, source, target) {
+    var t = '', n = input.split('').reduce((n,v)=>n * source.length + source.indexOf(v), 0);
+    do { t = target[n % target.length] + t;
+         n = Math.floor(n / target.length); 
+       } while (n > 0)
+    return t;
+  }
   
+// solution 2
+
+// const convert = (input, source, target) => {
+//     let dec = [...input].reduce((pre, val) => pre * source.length + source.indexOf(val), 0);
+//     let res = ``;
+//     do {
+//       res = target[dec % target.length] + res;
+//       dec = Math.floor(dec / target.length);
+//     } while (dec > 0);
+//     return res;
+//   };
+
 
 // solution 3
-// const isPrime = num => {
-//       for (let i = 2; i <= num ** .5; i++) {
-//         if (!(num % i)) return false;
-//       }
-//       return num > 1;
-//     }
+// function convert(inp, s, t) {
+//     var sl=s.length,tl=t.length,il=inp.length,d=[...inp].map((x,y)=>s.indexOf(x)*sl**(il-y-1)).reduce((x,y)=>x+y),li=[]
+//     while (d){ li.push(d%tl) ; d = parseInt(d/tl) }
+//     return li.reverse().map(x=>t[x]).join('') || t[0]
+// }
 // solution 4
 
-// function isPrime(num) {
-//       if (num < 2) {
-//          return false;
-//       }
-//     if (num === 2) {
-//          return true;
-//       }
-      
-//      const maxDiv = num**0.5 + 1;
-      
-//     for (let i = 2; i < maxDiv; i++) {
-//          if (num % i === 0) {
-//            return false;
-//          }
-//       }
-//     return true;
-//     }
+// function convert(input, source, target) {
+//     var t=0,r="";
+//     input.split('').forEach(cv=>{t=t*source.length;t=t+source.indexOf(cv);});
+//     do {r=target[t%target.length] + r;t=Math.floor(t/target.length);} while(t>0);
+//     return r;
+//   }
 
-console.log(isPrime(0));
+
+
+console.log(convert("15", dec, bin));
+
